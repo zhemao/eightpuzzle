e7ff5b1dc513d66032b34b2b73aa52b45e8c830e
Zhehao Mao <zhehao.mao@gmail.com>
2011-11-27 23:06:01 -0500

    update project dependencies

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]
-                 [org.clojure/math.combinatorics "0.0.2"]]
+                 [org.clojure/math.combinatorics "0.0.2"]
+                 [org.clojure/data.priority-map "0.0.1"]]
   :main eightpuzzle.core)
