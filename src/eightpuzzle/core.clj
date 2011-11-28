c78cece317fb7dd3002d44d78060c907e5ab7d08
Zhehao Mao <zhehao.mao@gmail.com>
2011-11-27 23:06:58 -0500

    It works!

--- src/eightpuzzle/core.clj
+++ src/eightpuzzle/core.clj
@@ -1,5 +1,6 @@
 (ns eightpuzzle.core
-  (:use [eightpuzzle.grid]))
+  (:use [eightpuzzle.grid]
+        [eightpuzzle.queue]))
 
 
 (defn branch-grid [grid]
@@ -11,4 +12,22 @@
     (if (> c 0) (swap! listref conj (shift-tile tiles [r (dec c)] [r c])))
     (if (< c 2) (swap! listref conj (shift-tile tiles [r (inc c)] [r c])))
     (map #(make-grid % (+ g 1)) @listref)))
+
+(defn solution? [grid]
+  (= 0 (:h grid)))
+
+(defn solve-puzzle [starting-tiles]
+  (loop [queue (make-grid-queue [(make-grid starting-tiles 0)])]
+    (let [grid (first-grid queue)]
+      (println (:h grid))
+      (if (solution? grid) grid
+        (let [newgrids (branch-grid grid)
+              newqueue (add-all-grids (pop queue) newgrids)]
+          (recur newqueue))))))
+
+(defn -main [& args]
+  (let [start [[3 6 1] [nil 8 2] [4 5 7]]
+        solution (solve-puzzle start)]
+    (println "Solved Puzzle!")
+    (println solution)))
 
